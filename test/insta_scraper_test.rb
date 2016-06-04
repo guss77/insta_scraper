3663714247ab931d35fa4dc9d0f721a5d152e90a
preciz <coolreef@gmail.com>
2016-06-04 16:25:31 +0200

    Remove example test

--- test/insta_scraper_test.rb
+++ test/insta_scraper_test.rb
@@ -4,8 +4,4 @@ class InstaScraperTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::InstaScraper::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
