c23e9f22ec68b873e83bf1a1581aaf1310fc409a
preciz <coolreef@gmail.com>
2016-06-05 18:39:13 +0200

    User proper variable name

--- test/scrapers/json/account_media_test.rb
+++ test/scrapers/json/account_media_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class InstaScraper::JSON::AccountMediaTest < Minitest::Test
   def setup
-    @media = InstaScraper::JSON::AccountMedia.new('barna.kovacs.codes')
+    @account_media = InstaScraper::JSON::AccountMedia.new('barna.kovacs.codes')
 
     json_file_path = File.expand_path('../samples/account_media.json', __FILE__)
     @json = JSON.parse(File.read(json_file_path))
@@ -15,14 +15,14 @@ class InstaScraper::JSON::AccountMediaTest < Minitest::Test
   end
 
   def test_account_url
-    assert @media.url == 'https://www.instagram.com/barna.kovacs.codes/media/'
+    assert @account_media.url == 'https://www.instagram.com/barna.kovacs.codes/media/'
   end
 
   def test_it_parses_json
-    assert @media.data == @json
+    assert @account_media.data == @json
   end
 
   def test_json_is_a_hashie_mash
-    assert @media.data.is_a? Hashie::Mash
+    assert @account_media.data.is_a? Hashie::Mash
   end
 end
