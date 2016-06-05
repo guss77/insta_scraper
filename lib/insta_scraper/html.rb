1c9164ae8e643f0c9459b376bd76380f6039a72d
preciz <coolreef@gmail.com>
2016-06-05 12:21:38 +0200

    Move sharedData logic to HTML class

--- lib/insta_scraper/html.rb
+++ lib/insta_scraper/html.rb
@@ -9,5 +9,23 @@ module InstaScraper
     def get_html
       open(url).read
     end
+
+    private
+
+    def line_with_data
+      html.each_line.detect { |l| l[/sharedData/] }
+    end
+
+    def shared_data
+      line_with_data[shared_data_start..shared_data_end]
+    end
+
+    def shared_data_start
+      (line_with_data =~ /sharedData/) + 13
+    end
+
+    def shared_data_end
+      line_with_data =~ /\};</
+    end
   end
 end
--- lib/insta_scraper/html/account.rb
+++ lib/insta_scraper/html/account.rb
@@ -19,23 +19,5 @@ module InstaScraper
                     .extend(Hashie::Extensions::DeepFetch)
                     .extend(Hashie::Extensions::DeepFind)
     end
-
-    private
-
-    def line_with_data
-      html.each_line.detect { |l| l[/sharedData/] }
-    end
-
-    def shared_data
-      line_with_data[shared_data_start..shared_data_end]
-    end
-
-    def shared_data_start
-      (line_with_data =~ /sharedData/) + 13
-    end
-
-    def shared_data_end
-      line_with_data =~ /\};</
-    end
   end
 end
