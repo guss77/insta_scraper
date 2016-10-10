0a75b97c96d5e2d41630e8bb47610fe61bfa7cd0
preciz <barna.kovacs.codes@gmail.com>
2016-10-10 14:40:06 +0200

    Remove setup method from account_media_test.rb
    Following tests would work without it, so keeping it consistent

--- test/scrapers/json/account_media_test.rb
+++ test/scrapers/json/account_media_test.rb
@@ -1,13 +1,6 @@
 require 'test_helper'
 
 class InstaScraper::JSON::AccountMediaTest < Minitest::Test
-  def setup
-    @account_media = InstaScraper::JSON::AccountMedia.new('barna.kovacs.codes')
-
-    json_file_path = File.expand_path('../samples/account_media.json', __FILE__)
-    @json = File.read(json_file_path)
-  end
-
   def test_it_raises_when_no_arguments
     assert_raises ArgumentError do
       InstaScraper::JSON::AccountMedia.new
@@ -15,10 +8,17 @@ class InstaScraper::JSON::AccountMediaTest < Minitest::Test
   end
 
   def test_account_url
+    @account_media = InstaScraper::JSON::AccountMedia.new('barna.kovacs.codes')
+
     assert @account_media.url == 'https://www.instagram.com/barna.kovacs.codes/media/'
   end
 
   def test_json_is_a_hashie_mash
+    @account_media = InstaScraper::JSON::AccountMedia.new('barna.kovacs.codes')
+
+    json_file_path = File.expand_path('../samples/account_media.json', __FILE__)
+    @json = File.read(json_file_path)
+
     @account_media.instance_variable_set('@raw_json', @json)
     assert @account_media.data.is_a? Hashie::Mash
   end
