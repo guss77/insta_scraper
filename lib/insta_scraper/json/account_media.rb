01d5d4ba41d010f88682909fdafed13995878913
preciz <barna.kovacs.codes@gmail.com>
2017-11-10 10:03:47 +0100

    Update AccountMedia to new endpoint

--- lib/insta_scraper/json/account_media.rb
+++ lib/insta_scraper/json/account_media.rb
@@ -1,15 +1,30 @@
 module InstaScraper
   class JSON::AccountMedia < InstaScraper::JSON
-    attr_reader :username,
-                :params
+    attr_reader :id, :per_page, :end_cursor
 
-    def initialize(username, params = {})
-      @username = username
-      @params = params
+    def initialize(id:, per_page: 12, end_cursor: nil)
+      @id = id
+      @per_page = per_page
+      @end_cursor = end_cursor
+    end
+
+    def variables
+      if end_cursor
+        "{\"id\":\"#{id}\",\"first\":#{per_page},\"after\":\"#{end_cursor}\"}"
+      else
+        "{\"id\":\"#{id}\",\"first\":#{per_page}}"
+      end
+    end
+
+    def params
+      {
+        query_id: 17888483320059182,
+        variables: variables,
+      }
     end
 
     def url
-      "https://www.instagram.com/#{username}/media/#{serialize_params}"
+      "https://www.instagram.com/graphql/query/#{serialize_params}"
     end
   end
 end
--- test/scrapers/json/account_media_test.rb
+++ test/scrapers/json/account_media_test.rb
@@ -8,24 +8,19 @@ class InstaScraper::JSON::AccountMediaTest < Minitest::Test
   end
 
   def test_account_url
-    @account_media = InstaScraper::JSON::AccountMedia.new('barna.kovacs.codes')
+    @account_media = InstaScraper::JSON::AccountMedia.new(id: 25025320)
 
-    assert @account_media.url == 'https://www.instagram.com/barna.kovacs.codes/media/'
+    assert @account_media.url ==
+      'https://www.instagram.com/graphql/query/?query_id=17888483320059182&variables={"id":"25025320","first":12}'
   end
 
   def test_json_is_a_hashie_mash
-    @account_media = InstaScraper::JSON::AccountMedia.new('barna.kovacs.codes')
+    @account_media = InstaScraper::JSON::AccountMedia.new(id: 25025320)
 
     json_file_path = File.expand_path('../samples/account_media.json', __FILE__)
     @json = File.read(json_file_path)
 
     @account_media.instance_variable_set('@raw_json', @json)
     assert @account_media.data.is_a? Hashie::Mash
-  end
-
-  def test_it_serializes_max_id_param
-    @account_media = InstaScraper::JSON::AccountMedia.new('barna.kovacs.codes', max_id: '1329031062285749945_528817151')
-
-    assert @account_media.url == 'https://www.instagram.com/barna.kovacs.codes/media/?max_id=1329031062285749945_528817151'
   end
 end
