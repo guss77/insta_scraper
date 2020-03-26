c20708dc60a37fc6b9f68e03588f2f66a4f1fa48
Oded Arbel <oded@geek.co.il>
2020-03-26 17:10:53 +0200

    add sensible default Faraday configuration

--- lib/insta_scraper.rb
+++ lib/insta_scraper.rb
@@ -17,3 +17,8 @@ require 'insta_scraper/json/media'
 require 'insta_scraper/json/account_media'
 require 'insta_scraper/json/media_comment'
 require 'insta_scraper/json/media_comment_stream'
+
+# configure Faraday with sensible defaults that make sense for a scrappper
+Faraday.default_connection.adapter Faraday.default_adapter
+Faraday.default_connection.use :cookie_jar
+Faraday.use Faraday::Response::RaiseError # we'd want to catch these errors and propagate logical exceptions
